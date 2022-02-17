f836a9780a11f3d9907bff0b644b6b58905f8591
Dmitry Simohin <symonf80@mail.ru>
2022-02-17 21:48:20 +0300

    add src

---
+++ src/Randoms.java
@@ -0,0 +1,31 @@
+import java.util.Iterator;
+import java.util.PrimitiveIterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected PrimitiveIterator.OfInt random;
+    protected int min, max;
+
+    public Randoms(int min, int max) {
+        random =  new Random().ints(min, (max + 1)).iterator();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt();
+            }
+        };
+    }
+
+
+}
