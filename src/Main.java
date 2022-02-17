fdc8dc61b4d3f6324feba841496d42e987c05ce7
symonf80 <94631730+symonf80@users.noreply.github.com>
2022-02-17 21:57:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        cycle();
+    }
+
+    private static void cycle() {
         for (int r : new Randoms(90, 100)) {
             System.out.println("Случайное число: " + r);
             if (r == 100) {
